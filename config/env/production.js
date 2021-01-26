0759cab603f82aba537002f8726891336bc1ffe5
Brian Foley <brianfoley@iMac.local>
2021-01-25 18:42:57 -0700

    configuration changes for production

--- config/env/production.js
+++ config/env/production.js
@@ -370,8 +370,8 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
-    internalEmailAddress: 'support@example.com',
+    baseUrl: 'https://allthingsgreat-writer.herokuapp.com/',
+    internalEmailAddress: 'foleyb25@gmail.com',
 
     // mailgunDomain: 'mg.example.com',
     // mailgunSecret: 'key-prod_fake_bd32301385130a0bafe030c',
--- config/sockets.js
+++ config/sockets.js
@@ -11,6 +11,7 @@
 
 module.exports.sockets = {
 
+  onlyAllowOrigins: []
   /***************************************************************************
   *                                                                          *
   * `transports`                                                             *
